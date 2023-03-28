638bc17e78ade15a4f78bb1be7db7bb2b24a404d
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2023-03-28 22:38:36 +0900

    [Silver III] Title: N과 M (7), Time: 676 ms, Memory: 124508 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] sel;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		sb = new StringBuilder();
+		
+		sel = new int[M];
+		
+		select(0);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	private static void select(int sidx) {
+		if(sidx==M) {
+			for(int i : sel) {
+				sb.append(i).append(" ");
+			}
+			sb.setLength(sb.length()-1);
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			sel[sidx] = arr[i];
+			select(sidx+1);
+		}
+	}
+}
