75bddf59f1be5b66ec8eed78c11f754eed29f4c9
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2023-12-03 00:56:39 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.15 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        
+        int answer = bridge_length;        
+        int sum = 0;
+        
+        Queue<Integer> bridge = new LinkedList<>();
+        for(int i=0; i<bridge_length; i++) {
+            bridge.add(0);
+        }
+        
+        Queue<Integer> trucks = new LinkedList<>();
+        for(int i : truck_weights) {
+            trucks.add(i);
+        }
+        
+        while(!trucks.isEmpty()) {
+            
+            if(bridge.peek() != 0) {
+                sum -= bridge.poll();
+            } else {
+                bridge.poll();
+            }
+            
+            if(sum + trucks.peek() <= weight) {
+                sum += trucks.peek();
+                bridge.offer(trucks.poll());
+            } else {
+                bridge.offer(0);
+            }
+
+            answer++;
+        }
+        
+        return answer;
+    }
+}
