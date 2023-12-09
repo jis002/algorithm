902c99dcfdcf52773e53c054d31cd621128eaab6
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2023-12-09 16:25:43 +0900

    [level 1] Title: 시저 암호, Time: 1.46 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -9,11 +9,11 @@ class Solution {
         for(char c : chars) {
             if(c==' ') {
                 sb.append(" ");
-            } else if(65 <= c && c <= 90) { // 대문자 범위 : 65~90
-                char newC = c+n <= 90 ? (char)(c+n) : (char)(c+n-26); 
+            } else if('A' <= c && c <= 'Z') { 
+                char newC = c+n <= 'Z' ? (char)(c+n) : (char)(c+n-26); 
                 sb.append(newC);
-            } else if(97 <= c && c <= 122) { // 소문자 범위 : 97~122
-                char newC = c+n <= 122 ? (char)(c+n) : (char)(c+n-26);
+            } else if('a' <= c && c <= 'z') {
+                char newC = c+n <= 'z' ? (char)(c+n) : (char)(c+n-26);
                 sb.append(newC);
             } 
         }      
