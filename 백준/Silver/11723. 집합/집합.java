c2c717af6cf8a2890f388b71dd8dd2737838e494
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2024-01-08 03:55:40 +0900

    [Silver V] Title: 집합, Time: 1144 ms, Memory: 300868 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,63 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		Set<Integer> set = new HashSet<>();
-		Set<Integer> setFull = new HashSet<>();
-		for(int i=1; i<=20; i++) {
-			setFull.add(i);
-		}
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String cmd = st.nextToken();
-			int x = 0;
-			
-			if(st.hasMoreTokens()) {
-				x = Integer.parseInt(st.nextToken());
-			}
-			
-			switch (cmd) {
-			case "add":
-				set.add(x);
-				break;
-			case "remove":
-				if(!set.isEmpty()) set.remove(x);
-				break;
-			case "check":
-				if(set.contains(x)) bw.write("1");
-				else bw.write("0");
-				bw.newLine();
-				break;
-			case "toggle":
-				if(set.contains(x)) set.remove(x);
-				else set.add(x);
-				break;
-			case "all":
-				set = setFull;
-				break;
-			case "empty": 
-				set.clear();
-				break;
-			}
-		}
-		
-		bw.close();
-		br.close();
-	} 
+    public static void main(String[] args) throws IOException {
 
+        Set<Integer> set = new HashSet<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int M = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            int x = 0;
+
+            if(st.hasMoreTokens()) {
+                x = Integer.parseInt(st.nextToken());
+            }
+
+            if(cmd.equals("add")) {
+                set.add(x);
+            } else if(cmd.equals("remove")) {
+                set.remove(x);
+            } else if(cmd.equals("check")) {
+                bw.write(set.contains(x) ? "1\n" : "0\n");
+            } else if(cmd.equals("toggle")) {
+                if(set.contains(x)) {
+                    set.remove(x);
+                } else {
+                    set.add(x);
+                }
+            } else if(cmd.equals("all")) {
+                for(int j=0; j<=20; j++) {
+                    set.add(j);
+                }
+            } else {
+                set.clear();
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
