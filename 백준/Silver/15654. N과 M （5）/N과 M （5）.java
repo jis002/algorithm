4ad889285b385f66632a4e0f7b4988f3036b411a
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2024-02-06 00:31:34 +0900

    [Silver III] Title: N과 M (5), Time: 340 ms, Memory: 29720 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,50 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-	
-	static int[] sel;
-	static boolean[] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		sel = new int[M];
-		visited = new boolean[N];
-		
-		permutation(0, N, M, arr);
-		
-	}
 
-	private static void permutation(int idx, int n, int m, int[] arr) {
-		if(idx==m) {
-			StringBuilder sb = new StringBuilder();
-			for(int i : sel) {
-				sb.append(i).append(" ");
-			}
-			sb.setLength(sb.length()-1);
-			System.out.println(sb.toString());
-			return;
-		}
-		
-		for(int i=0; i<n; i++) {
-			if(visited[i]) continue;
-			sel[idx] = arr[i];
-			visited[i] = true;
-			permutation(idx+1, n, m, arr);
-			visited[i] = false;
-		}
-	}
+    static int N;
+    static int M;
+    static int[] nums;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        selected = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        sb = new StringBuilder();
+        permutation(0);
+        System.out.println(sb.toString());
+    }
+
+    private static void permutation(int sidx) {
+        if(sidx == M) {
+            for(int num : selected) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if(visited[i]) continue;
+
+            visited[i] = true;
+            selected[sidx] = nums[i];
+            permutation(sidx+1);
+            visited[i] = false;
+        }
+    }
 }
