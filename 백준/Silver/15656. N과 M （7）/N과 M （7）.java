45b1fb0563e050f6b875bdb3d832491211bd21eb
Jisoo Kim <122420077+jis002@users.noreply.github.com>
2024-02-06 00:37:35 +0900

    [Silver III] Title: N과 M (7), Time: 636 ms, Memory: 123716 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,54 +1,48 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-	
-	static int N;
-	static int M;
-	static int[] arr;
-	static int[] sel;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		sb = new StringBuilder();
-		
-		sel = new int[M];
-		
-		select(0);
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString());
-		bw.close();
-	}
 
-	private static void select(int sidx) {
-		if(sidx==M) {
-			for(int i : sel) {
-				sb.append(i).append(" ");
-			}
-			sb.setLength(sb.length()-1);
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=0; i<N; i++) {
-			sel[sidx] = arr[i];
-			select(sidx+1);
-		}
-	}
+    static int N;
+    static int M;
+    static int[] nums;
+    static int[] selected;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        selected = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        sb = new StringBuilder();
+        permutation(0);
+        System.out.println(sb.toString());
+    }
+
+    private static void permutation(int sidx) {
+        if(sidx == M) {
+            for(int num : selected) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            selected[sidx] = nums[i];
+            permutation(sidx+1);
+        }
+    }
 }
